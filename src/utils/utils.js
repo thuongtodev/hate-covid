8f8034198ea4ca1797aab6e293a99c5e20524071
Tô Thành Thương <thuongthanhto@gmail.com>
2020-04-22 16:56:28 +0700

    chagne url

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,7 @@
 import { parse } from 'querystring';
 import pathRegexp from 'path-to-regexp';
 
-export const API_ENPOINT = 'https://up-backend-covid-1.herokuapp.com';
+export const API_ENPOINT = 'http://127.0.0.1:3000';
 
 /* eslint no-useless-escape:0 import/prefer-default-export:0 */
 const reg = /(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(?::\d+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/;
